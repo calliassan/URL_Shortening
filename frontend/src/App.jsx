ff4e9d180728a6b120ca712b90cdd0d23f480667
Ashish Bhattarai <ashishbhattarai49@gmail.com>
2026-04-02 14:19:16 +0530

    dashboard changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Register />} />
         <Route
           path="/dashboard"
-          element=<ProtectedRoute>{<Dashboard />} </ProtectedRoute>
+          element={<ProtectedRoute>{<Dashboard />} </ProtectedRoute>}
         />
       </Routes>
     </>
